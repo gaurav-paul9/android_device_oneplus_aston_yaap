51b9c0510596cd623503c6223a555df2cf1b0570
inferno0230 <mail@inferno0230.in>
2026-03-08 13:24:01 +0530

    aston: Update from OOS 11.F.28
    Change-Id: Id65a6cc7b4bef7b642d6df22db670dbe350d0627
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- lineage_aston.mk
+++ lineage_aston.mk
@@ -23,8 +23,8 @@ PRODUCT_MODEL := CPH2585
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="qssi-user 16 BP2A.250605.015 1765893596518 release-keys" \
-    BuildFingerprint=OnePlus/CPH2609EEA/OP5D35L1:16/TP1A.220905.001/U.R4T3.34e0c17-134bac4-1350b44:user/release-keys \
+    BuildDesc="qssi-user 16 BP2A.250605.015 1769251902777 release-keys" \
+    BuildFingerprint=OnePlus/CPH2585IN/OP5D35L1:16/TP1A.220905.001/U.R4T3.4ca61b8-2a66cd9-2a66cdc:user/release-keys \
     DeviceName=OP5D35L1 \
     DeviceProduct=CPH2585 \
     SystemDevice=OP5D35L1 \
